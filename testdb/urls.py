bd87612f071e0c898df9a52b60acb64fcb40eff4
mtong10 <mtong10@asu.edu>
2024-12-04 01:34:07 -0700

    Added Hitters and Pitchers page

--- testdb/urls.py
+++ testdb/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
 
     path("", views.playerinfo, name="player-info"),
+    path("hitters/", views.hitter_stats, name="hitter-stats"),
+    path("pitchers/", views.pitcher_stats, name="pitcher-stats"),
 
 ]
--- testdb/views.py
+++ testdb/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from testdb.models import Players
+from testdb.models import Players, Hitter, Pitcher
 import datetime
 
 def playerinfo(request):
     # now = datetime.datetime.now()
     # html = '<html lang="en"><body>It is now %s.</body></html>' % now
-
     p_objs = Players.objects.all()
-
     context = {
         "players": p_objs
     }
-    
     return render(request, "players_info.html", context)
+
+def hitter_stats(request):
+    h_objs = Hitter.objects.all().select_related('player_pid')
+    context = {"hitters": h_objs}
+    return render(request, "hitters_stats.html", context)
+
+def pitcher_stats(request):
+    p_objs = Pitcher.objects.all().select_related('player_pid')
+    context = {"pitchers": p_objs}
+    return render(request, "pitchers_stats.html", context)
